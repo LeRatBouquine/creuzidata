bd5288de131f0bacb31d16fd70a5314e757105a5
LeRatBouquine <41334724+LeRatBouquine@users.noreply.github.com>
2022-03-29 11:53:50 +0200

    Add files via upload

---
+++ my_streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+import pandas as pd
+
+import seaborn as sns
+
+import numpy as np
+
+
+st.title('Hello Wilders, welcome to my application!')
+
+
+st.write("Here you can find some of my projects (WIP)")
+
+
+print(np.version())
